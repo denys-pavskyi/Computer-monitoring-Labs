1b7ca0cfee8c93248d3bf1a5005f36f08f599757
Michail Krivov <miharol@ukr.net>
2024-05-23 01:05:26 +0300

    Add all models

--- website/__init__.py
+++ website/__init__.py
@@ -16,12 +16,24 @@ def create_app():
     from .point import point
     from .airstat import airstat
     from .waterstat import waterstat
+    from .soilstat import soilstat
+    from .radiationstat import radiationstat
+    from .waste import waste
+    from .economystat import economystat
+    from .healthstat import healthstat
+    from .energystat import energystat
 
     app.register_blueprint(point, url_prefix='/')
     app.register_blueprint(airstat, url_prefix='/')
     app.register_blueprint(waterstat, url_prefix='/')
+    app.register_blueprint(soilstat, url_prefix='/')
+    app.register_blueprint(radiationstat, url_prefix='/')
+    app.register_blueprint(waste, url_prefix='/')
+    app.register_blueprint(economystat, url_prefix='/')
+    app.register_blueprint(healthstat, url_prefix='/')
+    app.register_blueprint(energystat, url_prefix='/')
 
-    from .models import Point, AirStat, WaterStat
+    from .models import Point, AirStat, WaterStat, SoilStat, RadiationStat, Waste, EnergyStat, HealthStat, EnergyStat
     
     with app.app_context():
         db.create_all()
---
+++ website/radiationstat.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, jsonify
+
+from .models import RadiationStat
+from . import db
+
+radiationstat = Blueprint('radiationstat', __name__)
+
+@radiationstat.route('/points/<int:point_id>/radiationstat', methods=['POST'])
+def create_radiationstat_for_point(point_id):
+    data = request.get_json()
+    new_radiationstat = RadiationStat(
+        point_id=point_id, shortDecay=data.get('shortDecay'), mediumDecay=data.get('mediumDecay'),
+        air=data.get('air'), water=data.get('water')
+    )
+    db.session.add(new_radiationstat)
+    db.session.commit()
+    return jsonify(new_radiationstat.to_dict()), 201
+
+@radiationstat.route('/points/<int:point_id>/radiationstat', methods=['GET'])
+def get_radiationstat_for_point(point_id):
+    radiationstat = RadiationStat.query.filter_by(point_id=point_id).first_or_404()
+    return jsonify(radiationstat.to_dict())
+
+@radiationstat.route('/points/<int:point_id>/radiationstat/<int:radiationstat_id>', methods=['PUT'])
+def update_radiationstat_for_point(point_id, radiationstat_id):
+    radiationstat = RadiationStat.query.filter_by(point_id=point_id, id=radiationstat_id).first_or_404()
+    data = request.get_json()
+    radiationstat.shortDecay = data.get('shortDecay', radiationstat.shortDecay)
+    radiationstat.mediumDecay = data.get('mediumDecay', radiationstat.mediumDecay)
+    radiationstat.air = data.get('air', radiationstat.air)
+    radiationstat.water = data.get('water', radiationstat.water)
+    db.session.commit()
+    return jsonify(radiationstat.to_dict())
+
+@radiationstat.route('/points/<int:point_id>/radiationstat/<int:radiationstat_id>', methods=['DELETE'])
+def delete_radiationstat_for_point(point_id, radiationstat_id):
+    radiationstat = RadiationStat.query.filter_by(point_id=point_id, id=radiationstat_id).first_or_404()
+    db.session.delete(radiationstat)
+    db.session.commit()
+    return jsonify({'message': 'radiationstat deleted'})
