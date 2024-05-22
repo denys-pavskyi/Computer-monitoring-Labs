4c1658c87dc5369732b47eb8ad4ad79163664eeb
denys_pavskyi <den.pavski@gmail.com>
2024-05-22 22:32:07 +0300

    feature: Added loading points pre-load, fixed CORS error in API

--- ComputerMonitoringWeb/src/app/app.routes.ts
+++ ComputerMonitoringWeb/src/app/app.routes.ts
@@ -1,8 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { MapComponent } from './components/map/map.component';
 import { PointDetailsComponent } from './components/point-details/point-details.component';
 
 export const routes: Routes = [
     { path: '', component: MapComponent },
-    { path: 'point-details', component: PointDetailsComponent }
+    { path: 'point-details/:id', component: PointDetailsComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- ComputerMonitoringWeb/src/app/components/map/map.component.ts
+++ ComputerMonitoringWeb/src/app/components/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { Point } from '../../../models/point';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-map',
@@ -12,11 +14,12 @@ export class MapComponent implements OnInit {
   markers: L.Marker[] = [];
   customIcon: L.Icon | any;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.initMap();
     this.initCustomIcon();
+    this.loadPoints();
   }
 
   private initMap(): void {
@@ -33,12 +36,6 @@ export class MapComponent implements OnInit {
     this.map.on('click', this.onMapClick.bind(this));
   }
 
-  private onMapClick(e: any): void {
-    const marker = L.marker([e.latlng.lat, e.latlng.lng], { icon: this.customIcon }).addTo(this.map);
-    marker.bindPopup(this.createPopupContent(marker)).openPopup();
-    this.markers.push(marker);
-  }
-
   private initCustomIcon(): void {
     this.customIcon = L.icon({
       iconUrl: '/assets/free-icon-map-location-5092916.png',
@@ -48,25 +45,85 @@ export class MapComponent implements OnInit {
     });
   }
 
-  private createPopupContent(marker: L.Marker): HTMLElement {
+  private loadPoints(): void {
+    this.http.get<Point[]>('http://127.0.0.1:5000/points').subscribe(points => {
+      points.forEach(point => {
+        const marker = L.marker([point.cord1, point.cord2], { icon: this.customIcon }).addTo(this.map);
+        marker.bindPopup(this.createExistingPopupContent(marker, point)).openPopup();
+        this.markers.push(marker);
+      });
+    });
+  }
+
+  private onMapClick(e: any): void {
+    const marker = L.marker([e.latlng.lat, e.latlng.lng], { icon: this.customIcon }).addTo(this.map);
+    marker.bindPopup(this.createNewPopupContent(marker)).openPopup();
+    this.markers.push(marker);
+  }
+
+  private createNewPopupContent(marker: L.Marker): HTMLElement {
     const div = L.DomUtil.create('div', 'popup-content');
-    const title = L.DomUtil.create('h4', '', div);
-    title.innerHTML = 'New marker';
+    const titleInput = L.DomUtil.create('input', 'title-input', div);
+    titleInput.type = 'text';
+    titleInput.placeholder = 'Enter title';
+
+    const saveButton = L.DomUtil.create('button', 'save-button', div);
+    const saveIcon = L.DomUtil.create('img', 'save-icon', saveButton);
+    saveIcon.src = 'assets/diskette-icon.png';
+    saveIcon.alt = 'Save';
+    saveIcon.style.width = '16px';
+    saveIcon.style.height = '16px';
+    saveButton.appendChild(saveIcon);
+    saveButton.innerHTML += ' Save';
+    saveButton.onclick = () => {
+      this.saveMarkerData(marker, titleInput.value);
+      this.map.closePopup();
+    };
 
     const deleteButton = L.DomUtil.create('button', 'delete-button', div);
-    deleteButton.innerHTML = 'Видалити';
+    deleteButton.innerHTML = 'Delete';
     deleteButton.onclick = () => {
       this.removeMarker(marker);
+      this.map.closePopup();
+    };
+
+    return div;
+  }
+
+  private createExistingPopupContent(marker: L.Marker, point: Point): HTMLElement {
+    const div = L.DomUtil.create('div', 'popup-content');
+    const title = L.DomUtil.create('h4', '', div);
+    title.innerHTML = point.title;
+
+    const deleteButton = L.DomUtil.create('button', 'delete-button', div);
+    deleteButton.innerHTML = 'Delete';
+    deleteButton.onclick = () => {
+      this.removeMarker(marker);
+      if(point.id != undefined){
+        this.deletePoint(point.id);
+      }
       this.map.closePopup();
     };
 
     const detailsButton = L.DomUtil.create('button', 'details-button', div);
     detailsButton.innerHTML = 'Детальніше';
     detailsButton.onclick = () => {
-      alert('More details about this marker');
+      this.router.navigate(['/point-details', point.id]);
     };
 
     return div;
+  }
+
+  private saveMarkerData(marker: L.Marker, title: string): void {
+    const point: Point = {
+      title: title,
+      cord1: marker.getLatLng().lat,
+      cord2: marker.getLatLng().lng
+    };
+
+    this.http.post<Point>('/points', point).subscribe(savedPoint => {
+      marker.bindPopup(this.createExistingPopupContent(marker, savedPoint)).openPopup();
+    });
   }
 
   private removeMarker(marker: L.Marker): void {
@@ -75,5 +132,11 @@ export class MapComponent implements OnInit {
     if (index > -1) {
       this.markers.splice(index, 1);
     }
+  }
+
+  private deletePoint(id: number): void {
+    this.http.delete(`/points/${id}`).subscribe(() => {
+      console.log(`Point with id ${id} deleted`);
+    });
   }
 }
